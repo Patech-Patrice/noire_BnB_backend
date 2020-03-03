d220283a4a442aecff90b0709aad254a549740d0
Patrice Drayton <patricedrayton@Patrices-MacBook-Pro.local>
2020-03-03 18:58:27 -0500

    added functionality to controllers

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -3,15 +3,38 @@ class Api::V1::CommentsController < ApplicationController
 
     def index
         @comments = Comment.all
-
         render json: @comments
     end
 
-    def show
+    # GET /comments/1
+  def show
+    render json: @comment
+  end
+
+  # POST /comments
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render json: @comment, status: :created, location: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
     end
 
-    def create
+    # Only allow a trusted parameter "white list" through.
+    def comment_params
+      params.require(:comment).permit(:content, :destination_id, :attraction_id)
     end
-
 
 end
--- app/controllers/api/v1/destinations_controller.rb
+++ app/controllers/api/v1/destinations_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::DestinationsController < ApplicationController
 
     def index
         @destinations = Destination.all
-
         render json: @destinations
     end
 
